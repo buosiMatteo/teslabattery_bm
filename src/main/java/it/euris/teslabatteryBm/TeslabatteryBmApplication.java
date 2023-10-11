a7a7dea18b243ac9c69c445d73d722c3acca1b73
matt <buosi.matteo@gmail.com>
2023-10-11 10:10:43 +0200

    First Commit

---
+++ src/main/java/it/euris/teslabatteryBm/TeslabatteryBmApplication.java
@@ -0,0 +1,13 @@
+package it.euris.teslabatteryBm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeslabatteryBmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeslabatteryBmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/euris/teslabatteryBm/TeslabatteryBmApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.teslabatteryBm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslabatteryBmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
